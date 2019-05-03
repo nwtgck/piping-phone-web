96782f2f175a0094407c9f4c407af9caedaf45b5
Ryo Ota <nwtgck@gmail.com>
2019-05-03 22:36:51 +0900

    [add] Create a basic record-and-play

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,6 +33,72 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import MediaStreamRecorder from 'msr';
+
+(window as any).AudioContext = (window as any).AudioContext || (window as any).webkitAudioContext;
+
+console.log(MediaStreamRecorder);
+
+function blobToArrayBuffer(blob: Blob): Promise<ArrayBuffer> {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      resolve(fileReader.result as ArrayBuffer);
+    };
+    fileReader.onerror = () => {
+      reject(fileReader.error);
+    };
+    fileReader.readAsArrayBuffer(blob);
+  });
+}
+
+
+navigator.getUserMedia({audio: true}, onMediaSuccess, (err) => console.error((err)));
+
+
+function onMediaSuccess(stream: MediaStream): void {
+  const mediaRecorder = new MediaStreamRecorder(stream);
+  const blobs: Blob[] = [];
+  mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+  console.log('record start');
+  mediaRecorder.ondataavailable = (blob: Blob) => {
+    blobs.push(blob);
+    console.log(blob);
+
+    if (blobs.length === 100) {
+      (async () => {
+        console.log("record end");
+        mediaRecorder.stop();
+        console.log("play start");
+
+        const context = new AudioContext();
+        console.log('context.currentTime', context.currentTime);
+        let prevStartTime = 0;
+        let prevDuration = 0;
+
+        for (const blob of blobs) {
+          console.log(blob);
+          const arrayBuffer = await blobToArrayBuffer(blob);
+          console.log('arraybuffer', arrayBuffer);
+          const audioBuffer = await context.decodeAudioData(arrayBuffer);
+          console.log('audio buffer', audioBuffer);
+          console.log('audio buffer duration', audioBuffer.duration);
+          const source = context.createBufferSource();
+          source.buffer = audioBuffer;
+          source.connect(context.destination);
+          const currentTime = context.currentTime;
+          const startTime = Math.max(prevDuration - (currentTime - prevStartTime), 0);
+          console.log('startTime', startTime);
+          prevStartTime = currentTime + startTime;
+          prevDuration = audioBuffer.duration;
+          source.start(startTime);
+        }
+      })();
+    }
+  };
+
+  mediaRecorder.start(100 * 2);
+}
 
 @Component
 export default class HelloWorld extends Vue {
