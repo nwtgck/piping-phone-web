21109d60afaa3067ea93fd3f37058e0c4e254e6e
Ryo Ota <nwtgck@gmail.com>
2019-05-04 09:39:00 +0900

    [change] Rename HelloWorld to PipingPhone

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Piping Phone</h1>
+    <PipingPhone />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import PipingPhone from './components/PipingPhone.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    PipingPhone: PipingPhone,
   },
 })
 export default class App extends Vue {}
